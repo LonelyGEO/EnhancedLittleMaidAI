12a3d9850872d4cb29f370df56e1816c9ede24de
CheukWang Leung <2964476948des@gmail.com>
2026-05-01 06:14:50 +1000

    fix: 少女寻友中气泡仅 debugLog=true 时显示

--- src/main/java/com/github/lonelygeo/enhancedlittlemaidai/mixin/EntityMaidMixin.java
+++ src/main/java/com/github/lonelygeo/enhancedlittlemaidai/mixin/EntityMaidMixin.java
@@ -218,17 +218,17 @@ public abstract class EntityMaidMixin {
                         if (EnhancedConfig.debugLog()) {
                             EnhancedLittleMaidAI.LOGGER.info(
                                     "InterMaidChat: Maid {} proposed to {} targets", uuid, targets.size());
+                            long scanBubbleId = maid.getChatBubbleManager()
+                                    .addTextChatBubble("**少女寻友中...**");
+                            new Thread(() -> {
+                                try { Thread.sleep(5000); } catch (InterruptedException ignored) {}
+                                net.minecraft.server.MinecraftServer srv = maid.getServer();
+                                if (srv != null && !maid.isRemoved()) {
+                                    srv.submit(() -> maid.getChatBubbleManager()
+                                            .removeChatBubble(scanBubbleId));
+                                }
+                            }).start();
                         }
-                        long scanBubbleId = maid.getChatBubbleManager()
-                                .addTextChatBubble("**少女寻友中...**");
-                        new Thread(() -> {
-                            try { Thread.sleep(5000); } catch (InterruptedException ignored) {}
-                            net.minecraft.server.MinecraftServer srv = maid.getServer();
-                            if (srv != null && !maid.isRemoved()) {
-                                srv.submit(() -> maid.getChatBubbleManager()
-                                        .removeChatBubble(scanBubbleId));
-                            }
-                        }).start();
                     } else if (EnhancedConfig.debugLog()) {
                         EnhancedLittleMaidAI.LOGGER.debug(
                                 "InterMaidChat: density skip, partners={}", partners.size());
